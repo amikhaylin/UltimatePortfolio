75fbc26d27f3926e30f989eb44fdeaa2dbd52ed3
Andrey Mikhaylin <cenobith@gmail.com>
2021-06-17 12:56:35 +0300

    Created data model

--- UltimatePortfolio/UltimatePortfolioApp.swift
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -9,9 +9,18 @@ import SwiftUI
 
 @main
 struct UltimatePortfolioApp: App {
+    @StateObject var dataController: DataController
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
+                .environmentObject(dataController)
         }
+    }
+    
+    init() {
+        let dataController = DataController()
+        _dataController = StateObject(wrappedValue: dataController)
     }
 }
