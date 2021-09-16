1ff8629d74955bbbabd66188c14701fb693b0e12
Andrey Mikhaylin <cenobith@gmail.com>
2021-09-16 17:20:54 +0300

    uncompleted

---
+++ PortfolioWidget/PortfolioWidget.swift
@@ -0,0 +1,68 @@
+//
+//  PortfolioWidget.swift
+//  PortfolioWidget
+//
+//  Created by Andrey Mikhaylin on 15.09.2021.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date(), items: [Item.example])
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> Void) {
+        let entry = SimpleEntry(date: Date(), items: [Item.example])
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> Void) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate, items: [Item.example])
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+    let items: [Item]
+}
+
+struct PortfolioWidgetEntryView: View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        Text(entry.date, style: .time)
+    }
+}
+
+@main
+struct PortfolioWidget: Widget {
+    let kind: String = "PortfolioWidget"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            PortfolioWidgetEntryView(entry: entry)
+        }
+        .configurationDisplayName("My Widget")
+        .description("This is an example widget.")
+    }
+}
+
+struct PortfolioWidget_Previews: PreviewProvider {
+    static var previews: some View {
+        PortfolioWidgetEntryView(entry: SimpleEntry(date: Date(), items: [Item.example]))
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
+    }
+}
