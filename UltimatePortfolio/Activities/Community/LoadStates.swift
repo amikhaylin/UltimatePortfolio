bffcd7b649a11c2fe679ffcab7b51aba548a6b0a
Andrey Mikhaylin <cenobith@gmail.com>
2021-09-28 09:48:51 +0300

    Querying data from iCloud

---
+++ UltimatePortfolio/Activities/Community/LoadStates.swift
@@ -0,0 +1,12 @@
+//
+//  LoadStates.swift
+//  UltimatePortfolio
+//
+//  Created by Andrey Mikhaylin on 27.09.2021.
+//
+
+import Foundation
+
+enum LoadState {
+    case inactive, loading, success, noResults
+}
--- UltimatePortfolio/Activities/ContentView.swift
+++ UltimatePortfolio/Activities/ContentView.swift
@@ -42,6 +42,13 @@ struct ContentView: View {
                     Image(systemName: "rosette")
                     Text("Awards")
                 }
+            
+            SharedProjectsView()
+                .tag(SharedProjectsView.tag)
+                .tabItem {
+                    Image(systemName: "person.3")
+                    Text("Community")
+                }
         }
         .onContinueUserActivity(CSSearchableItemActionType, perform: moveToHome)
         .userActivity(newProjectActivity) { activity in
